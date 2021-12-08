c6cefdf2a4e26e06c47f83dde5db5529c268b9bb
ATEOTG <bryansantos9797@gmail.com>
2021-12-07 19:02:22 -0500

    added conditional to do nothing if there's no books submitted by user

--- src/components/Generator.js
+++ src/components/Generator.js
@@ -17,10 +17,17 @@ class Generator extends Component {
         let BookPosition = Math.floor(Math.random() * this.props.bookdata.length);
         let BookResult = this.props.bookdata[BookPosition];
         
+        //if there's nothing added do nothing
+        if (this.props.bookdata.length == 0) {
+            return 
+        }
+
+        //else we insert the book in the beginning of our result variable list
+        else (
         this.setState( (state) => {
             return state.result.splice(0,0,BookResult)
-        }
-        );
+        }));
+        
         
         
         //alert("The Next Book You're Going To Read Is " + this.props.bookdata[BookPosition][0]  +
@@ -34,8 +41,9 @@ class Generator extends Component {
                
              <button id = "Generate" onClick = {this.result}> Generate </button>
             
-             <h1>The Next Book You're Going To Read Is: {this.state.result[0][0]} by {this.state.result[0][1]}
-                </h1>
+             <h1>The Next Book You're Going To Read Is </h1>
+             <b></b>
+             <h2> {this.state.result[0][0]} by {this.state.result[0][1]} </h2>
             
              </div>
 
